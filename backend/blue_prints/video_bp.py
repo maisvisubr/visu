da7906d609e89ffb0aa461ff958fde63cc8e6c35
gusta <gustavo024dias@gmail.com>
2023-08-15 00:12:22 -0300

    databse configurado

--- backend/blue_prints/video_bp.py
+++ backend/blue_prints/video_bp.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
+from pymongo import MongoClient
 
 video_bp = Blueprint("video_bp", __name__)
 
+client = MongoClient("mongodb+srv://maisvisubr:sHQu2YbLUGEvgruD@cluster0.um6smo3.mongodb.net/")
+db = client["db"]
+col_videos = db["videos"]
 
 @video_bp.route("/get_videos", methods=["POST"])
 def get_videos():
-    return jsonify([])
+    return jsonify([v for v in col_videos.find()])
