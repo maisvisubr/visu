e42ab25c6d2e5deb62b7ca2a42c5f472c41b3cc4
gusta <gustavo024dias@gmail.com>
2023-08-15 00:14:08 -0300

    show versions

--- backend/blue_prints/video_bp.py
+++ backend/blue_prints/video_bp.py
@@ -7,6 +7,12 @@ client = MongoClient("mongodb+srv://maisvisubr:sHQu2YbLUGEvgruD@cluster0.um6smo3
 db = client["db"]
 col_videos = db["videos"]
 
+
+@video_bp.route("/")
+def show_version():
+    return jsonify({"version": 1.1})
+
+
 @video_bp.route("/get_videos", methods=["POST"])
 def get_videos():
     return jsonify([v for v in col_videos.find()])
